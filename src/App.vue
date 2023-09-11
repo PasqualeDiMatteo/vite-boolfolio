131024a80485d3cbd5881a0ea18db1f91f0c3d9a
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-09-11 15:43:39 +0200

    added call api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <script >
+import axios from "axios";
 import AppHeader from './components/AppHeader.vue';
+const endpoint = "http://127.0.0.1:8000/api/projects";
 export default {
-  components: { AppHeader }
+  components: { AppHeader },
+  data() {
+    return {
+      projects: [],
+      links: [],
+    }
+  },
+  methods: {
+    fetchProject() {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data.data;
+        this.links = res.data.links;
+      })
+    }
+  },
+  mounted() {
+    this.fetchProject()
+  }
 }
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    <ul>
+      <li v-for="project in projects">{{ project.title }}</li>
+    </ul>
+  </main>
 </template>
 
 <style ></style>
