668ffb9fff4fd00d7a301ac005d42bb83912e1f3
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-09-12 16:33:56 +0200

    added link on navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,7 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <router-link class="nav-link active" :to="{ name: 'home' }">Home</router-link>
                         </li>
                     </ul>
                 </div>
